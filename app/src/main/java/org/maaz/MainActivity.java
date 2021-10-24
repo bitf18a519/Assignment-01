ebe42779b299e4e4a367560786fe16270cb41da0
bitf18a519 <bitf18a519@pucit.edu.pk>
2021-10-24 11:56:48 +0500

    textview id set

--- app/src/main/java/org/maaz/MainActivity.java
+++ app/src/main/java/org/maaz/MainActivity.java
@@ -3,6 +3,7 @@ package org.maaz;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,10 +12,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //Adding reference
+
         EditText num1 =(EditText) findViewById(R.id.num1c);
         EditText num2=(EditText) findViewById(R.id.num2g);
         EditText num3=(EditText) findViewById(R.id.num3s);
         EditText num4=(EditText) findViewById(R.id.num4sg);
+
+
 
     }
 }
